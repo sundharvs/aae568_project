d4bcf4c79df52ca2fa359d4584a573362743fa9f
Sundhar Vinodh Sangeetha <sundharvs@gmail.com>
2024-04-18 01:20:16 -0400

    fix px4 odometry qos

--- aae568_project/radar.py
+++ aae568_project/radar.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from px4_msgs.msg import VehicleOdometry
 from geometry_msgs.msg import Point
-from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+from rclpy.qos import qos_profile_sensor_data
 import math
 
 class Radar(Node):
@@ -12,12 +12,7 @@ class Radar(Node):
         self.odometry_subscriber_ = self.create_subscription(
             VehicleOdometry,
             "/fmu/out/vehicle_odometry",
-            self.odometry_callback,
-            QoSProfile(
-                reliability=QoSReliabilityPolicy.BEST_EFFORT,
-                history=QoSHistoryPolicy.UNKNOWN,
-                durability=QoSDurabilityPolicy.TRANSIENT_LOCAL
-            )
+            self.odometry_callback, qos_profile_sensor_data
         )
         self.publisher_ = self.create_publisher(Point, 'radar', 10)
 
@@ -26,7 +21,7 @@ class Radar(Node):
         y = odometry_msg.position[1]
         z = odometry_msg.position[2]
 
-        r = math.sqrt(x^2 + y^2 + z^2)
+        r = math.sqrt(x**2 + y**2 + z**2)
         theta = math.atan2(y,x)
         phi = math.acos(z/r)
 
@@ -35,14 +30,16 @@ class Radar(Node):
         msg.y = theta
         msg.z = phi
 
+        self.publisher_.publish(msg)
+
 def main(args=None):
     rclpy.init(args=args)
 
-    Radar = Radar()
+    radar = Radar()
 
-    rclpy.spin(Radar)
+    rclpy.spin(radar)
 
-    Radar.destroy_node()
+    radar.destroy_node()
     rclpy.shutdown()
 
 
